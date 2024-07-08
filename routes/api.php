cdb84452c6defc8dafefde7894a7a7768f8a934c
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-07-08 23:45:08 +0100

    fixed the routing

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+});
 
 Route::middleware(['auth:api'])->group(function () {
     // Protected routes
